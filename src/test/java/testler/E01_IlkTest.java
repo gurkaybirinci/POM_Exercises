0f00377c33306cc7fe41213e4257a0c7e7523182
Gürkay Birinci <117175953+gurkaybirinci@users.noreply.github.com>
2023-02-01 16:31:38 +0300

    Revert "v.1"

--- src/test/java/testler/E01_IlkTest.java
+++ src/test/java/testler/E01_IlkTest.java
@@ -10,12 +10,11 @@ public class E01_IlkTest {
     @Test
     public void test01() {
         Driver.getDriver().get("https://www.google.com");
-        // Google Anasayfası
+
         GoogleHome googleHome = new GoogleHome();
         googleHome.search.sendKeys("iphone");
         googleHome.button.click();
 
-        //Google Arama Sonuç Sayfası
         GoogleResult googleResult = new GoogleResult();
         googleResult.gorsellerButonu.click();
         googleResult.alisverisButonu.click();
